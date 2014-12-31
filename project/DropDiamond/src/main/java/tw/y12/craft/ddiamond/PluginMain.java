ec3f51295b1fc3f8947a36baa013782c50285cb1
y12studio <y12studio@gmail.com>
2014-12-31 11:45:00 +0800

    ref #1 bug fixed

--- project/DropDiamond/src/main/java/tw/y12/craft/ddiamond/PluginMain.java
+++ project/DropDiamond/src/main/java/tw/y12/craft/ddiamond/PluginMain.java
@@ -6,29 +6,30 @@ import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PluginMain extends JavaPlugin{
-	
+public class PluginMain extends JavaPlugin {
+
 	private static Plugin _pi;
 
-	private void addRecipe(){
+	private void addRecipes() {
 
-		ShapedRecipe yRecipeDiamond=new ShapedRecipe(new ItemStack(Material.DIAMOND,1));
-		  yRecipeDiamond.shape("D D","DCD"," C ");
-		  yRecipeDiamond.setIngredient('D',Material.DIRT);
-		  yRecipeDiamond.setIngredient('C',Material.COBBLESTONE);
-		  getServer().addRecipe(yRecipeDiamond);
-		 
-		  ShapedRecipe yRecipeTnt=new ShapedRecipe(new ItemStack(Material.TNT,1));
-		  yRecipeTnt.shape("W W","WCW"," C ");
-		  yRecipeTnt.setIngredient('W',Material.WOOD);
-		  yRecipeTnt.setIngredient('C',Material.COBBLESTONE);
-		  getServer().addRecipe(yRecipeTnt);
+		ShapedRecipe yRecipeDiamond = new ShapedRecipe(new ItemStack(
+				Material.DIAMOND, 1));
+		yRecipeDiamond.shape("D D", "DCD", " C ");
+		yRecipeDiamond.setIngredient('D', Material.DIRT);
+		yRecipeDiamond.setIngredient('C', Material.COBBLESTONE);
+		getServer().addRecipe(yRecipeDiamond);
+
+		ShapedRecipe yRecipeTnt = new ShapedRecipe(new ItemStack(
+				Material.SULPHUR, 4)).shape("W W", "WCW", " C ")
+				.setIngredient('W', Material.WOOD)
+				.setIngredient('C', Material.COBBLESTONE);
+		getServer().addRecipe(yRecipeTnt);
 	}
 
 	public void onEnable() {
 		_pi = this;
 		getServer().getPluginManager().registerEvents(new DropEvent(), this);
-		addRecipe();
+		addRecipes();
 	}
 
 	public static Plugin getPlugin() {
